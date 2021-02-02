265881b8d92d92ac66a0bb8e390dab42c034dfc2
Goncalo Oliveira <goncalo.oliveira@redpanda.sh>
2021-02-02 13:35:19 +0100

    added aspnet-api template

---
+++ template/aspnet-api/api/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace OpenFaaS
+{
+    public class Startup
+    {
+        public Startup( IConfiguration configuration )
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices( IServiceCollection services )
+        {
+            // add your services here.
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure( IApplicationBuilder app, bool isDevelopmentEnv )
+        {            
+        }
+    }
+}
