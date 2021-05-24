6a4ec5324c134fa74e14b69ec66d14d4d1d9f36e
Goncalo Oliveira <goncalo.a.oliveira@gmail.com>
2021-05-24 10:48:58 +0200

    allow private nuget sources without username/password

--- template/aspnet/add-package-source.sh
+++ template/aspnet/add-package-source.sh
@@ -5,7 +5,9 @@
 # $3: password
 
 if [ -n "$1" ] && [ -n "$2" ] && [ -n "$3" ]; then
-    echo "--source $1 --username $2 --password $3"
-    # dotnet nuget add source 
+    # dotnet nuget add source with username and password
     dotnet nuget add source -n private -u $2 -p $3 --store-password-in-clear-text $1
+elif [ -n "$1" ]; then
+    # dotnet nuget add source
+    dotnet nuget add source -n private $1
 fi
